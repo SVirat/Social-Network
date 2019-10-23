496fb3e11a978a1679ca923c2bc83f2fe97a2460
Virat Singh <SVirat@users.noreply.github.com>
2019-10-23 14:26:09 +0530

    Added connection variables for Heroku

--- config.php
+++ config.php
@@ -3,7 +3,14 @@ ob_start();
 session_start();
 $timezone = date_default_timezone_set("Asia/Calcutta");
 
-$con = mysqli_connect("localhost", "root", "", "user");
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$con = mysqli_connect($server, $username, $password, $db);
 
 if(mysqli_connect_errno()) {
     echo "Failure: " . mysqli_connect_errno();
