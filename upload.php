485eedd4fd20f64a63d539a166322bd99ecc0aaa
Virat Singh <SVirat@users.noreply.github.com>
2019-10-24 20:20:18 +0530

    Changed region

--- upload.php
+++ upload.php
@@ -5,7 +5,7 @@ include ("inc/classes/User.php");
 // this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
 $s3 = new Aws\S3\S3Client([
     'version'  => 'latest',
-    'region'   => 'us-east-2',
+    'region'   => 'ap-south-1',
 ]);
 $bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
 $user_handle =  $user['handle'];
