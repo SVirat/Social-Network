d5e07bdc078d7646d04addbe2fd383ba803b49eb
Virat Singh <SVirat@users.noreply.github.com>
2019-10-24 19:31:53 +0530

    Added AWS

--- upload.php
+++ upload.php
@@ -1,5 +1,14 @@
-<?php include("inc/header.php");
+<?php 
+require('vendor/autoload.php');
+include("inc/header.php");
 include ("inc/classes/User.php");
+
+// this will simply read AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from env vars
+$s3 = new Aws\S3\S3Client([
+    'version'  => '2006-03-01',
+    'region'   => 'us-east-1',
+]);
+$bucket = getenv('S3_BUCKET')?: die('No "S3_BUCKET" config var in found in env!');
 
 $user_handle =  $user['handle'];
 $target_dir = "assets/img/user_prof_pics/$user_handle/";
@@ -46,6 +55,8 @@ if ($uploadOk == 0) {
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        $upload = $s3->upload($bucket, $_FILES['userfile']['name'], fopen($_FILES['userfile']['tmp_name'], 'rb'), 'public-read');
+?>
         $user_obj = new User($con, $user_handle);
         $user_obj->set_profile_pic("./" . $target_dir . basename( $_FILES["fileToUpload"]["name"]));
         header("Location: settings.php");
